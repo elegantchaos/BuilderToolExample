2fa97be8571aa2ec601303fa9796a0b54ccd5d14
Sam Deane <sam@elegantchaos.com>
2018-03-02 00:18:16 +0000

    add an executable product

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "BuilderToolExample",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .executable(
+            name: "BuilderToolExample",
+            targets: ["BuilderToolExample"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
